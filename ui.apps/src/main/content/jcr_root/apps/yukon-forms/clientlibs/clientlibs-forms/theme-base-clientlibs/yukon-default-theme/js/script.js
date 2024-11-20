343276a47e3e8e146f5acd774e97f26e9952e8f5
Nelson Wei <nelson@ensemble.com>
2024-11-20 14:53:35 -0800

    Font fix

--- ui.apps/src/main/content/jcr_root/apps/yukon-forms/clientlibs/clientlibs-forms/theme-base-clientlibs/yukon-default-theme/js/script.js
+++ ui.apps/src/main/content/jcr_root/apps/yukon-forms/clientlibs/clientlibs-forms/theme-base-clientlibs/yukon-default-theme/js/script.js
@@ -1,19 +1,18 @@
+/**
+ * I don't believe script gets loaded here, it seems to only load the style/css to the theme.
+ * We'll need to move this somewhere else, probably mnove it to another clientlib and call it from page component
+ *
+ */
+
 var _paq = window._paq = window._paq || [];
 
 _paq.push(['trackPageView']);
-
 _paq.push(['enableLinkTracking']);
 
 (function() {
-
     var u="https://analytics.gov.yk.ca/";
-
     _paq.push(['setTrackerUrl', u+'matomo.php']);
-
     _paq.push(['setSiteId', '4']);
-
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-
     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-
 })();
