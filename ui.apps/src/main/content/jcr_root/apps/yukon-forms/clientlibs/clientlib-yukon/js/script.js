4e61559fe5ad1af4d3ba9f5cdfbe9193010d2621
Nelson Wei <nelson@ensemble.com>
2024-11-20 16:13:48 -0800

    Custom js script initial commit

---
+++ ui.apps/src/main/content/jcr_root/apps/yukon-forms/clientlibs/clientlib-yukon/js/script.js
@@ -0,0 +1,9 @@
+/**
+ * Load Custom Script
+ * @name loadCustomScript Load Custom Script
+ * @retiurn boolean
+ */
+function loadCustomScript() {
+    console.debug("Loading custom script...");
+    return true;
+}
