b71b7a456a2c533d7f9194211e4bcba4828b36a2
anai <anai@ensemble.com>
2024-12-03 12:43:54 +0000

    Updated the custom Matomo script. Configured the template to automatically include the clientLibRef and rule.

--- ui.apps/src/main/content/jcr_root/apps/yukon-forms/clientlibs/clientlib-yukon/js/script.js
+++ ui.apps/src/main/content/jcr_root/apps/yukon-forms/clientlibs/clientlib-yukon/js/script.js
@@ -1,9 +1,30 @@
 /**
- * Load Custom Script
- * @name loadCustomScript Load Custom Script
- * @retiurn boolean
+ * Load Custom Matomo Script
+ * @name loadCustomMatomoScript Load Custom Matomo Script
  */
-function loadCustomScript() {
-    console.debug("Loading custom script...");
-    return true;
+
+var _paq = window._paq = window._paq || [];
+
+_paq.push(['trackPageView']);
+
+_paq.push(['enableLinkTracking']);
+
+function loadCustomMatomoScript(siteId) {
+    var u='https://analytics.gov.yk.ca/';
+
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+
+    _paq.push(['setSiteId', siteId]);
+
+    var d=document;
+
+    var g=d.createElement('script');
+
+    var s=d.getElementsByTagName('script')[0];
+
+    g.async=true;
+
+    g.src=u+'matomo.js';
+
+    s.parentNode.insertBefore(g,s);
 }
