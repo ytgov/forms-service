06c8ce518088281f5ea1c7fd82658830a6042d50
Haram Lee <haraml@ensemble.com>
2025-03-17 13:25:25 -0700

    added address component to have limit field

--- core/src/main/java/ca/yukon/aem/core/models/AddressModel.java
+++ core/src/main/java/ca/yukon/aem/core/models/AddressModel.java
@@ -3,6 +3,8 @@ package ca.yukon.aem.core.models;
 import ca.yukon.aem.core.forms.services.impl.CanadaPostApiService;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -14,6 +16,9 @@ public class AddressModel {
     protected CanadaPostApiService canadaPostApiService;
     private String key = "";
     private String host = "";
+    @ValueMapValue
+    @Optional
+    private int limit = 7;
 
     @PostConstruct
     protected void init() {
@@ -29,5 +34,9 @@ public class AddressModel {
 
     public String getKey() {
         return this.key;
+    }
+
+    public int getLimit() {
+        return this.limit;
     }
 }
